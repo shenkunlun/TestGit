f663c2bfb542b89cc2816d1e3ae35a405f83c2ce
Kunlun Shen <kunlun_shen@hotmail.com>
2018-10-05 12:32:39 +1000

    first commit

---
+++ TestGit/src/com/me/Test1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.me;
+
+/**
+ * @author Kunlun Shen
+ *
+ */
+public class Test1 {
+
+	/**
+	 * 
+	 */
+	public Test1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
